97026d1a50287f4ae0ebb73a84bb29c077440920
Fernanda Gabrielli <123499117+FernandaGabrielli@users.noreply.github.com>
2025-01-28 00:47:46 -0300

    FirstOne

---
+++ 01.py
@@ -0,0 +1,12 @@
+# Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;  
+INDICE = 13
+SOMA = 0
+K = 0
+# Enquanto K for menor que INDICE, vamos somando
+while K < INDICE:
+    K = K + 1  # Aumenta o valor de K
+    SOMA = SOMA + K  # Soma o valor de K em SOMA
+
+print(SOMA)  # No final, imprime o valor total
+# Ao final do processamento, qual será o valor da variável SOMA? 
+# Retornou 91.
